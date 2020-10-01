1f2becbde694314ceefd6c7a3accecea17300e68
manju_minji <0923alswl@gmail.com>
2020-10-01 21:04:37 +0900

    Starter Code 추가

--- exercises/word-count/Sources/WordCount/WordCount.swift
+++ exercises/word-count/Sources/WordCount/WordCount.swift
@@ -1,1 +1,10 @@
 //Solution goes in Sources
+class WordCount {
+    var words:String
+    init(words:String) {
+        self.words = words
+    }
+    func count() -> Dictionary<String , Int> {
+        /* write your code here */
+    }
+}

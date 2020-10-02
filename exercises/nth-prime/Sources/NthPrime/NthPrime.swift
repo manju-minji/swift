32982fa0e9429fc5451d005507e7fa893bf1f428
manju_minji <0923alswl@gmail.com>
2020-10-02 22:46:33 +0900

    Starter Code 추가

--- exercises/nth-prime/Sources/NthPrime/NthPrime.swift
+++ exercises/nth-prime/Sources/NthPrime/NthPrime.swift
@@ -1,1 +1,17 @@
 //Solution goes in Sources
+class Prime {
+    static func nth(_ n: Int) -> Int? {
+        if n <= 0 { return nil }
+        
+        /* write your code here */
+    }
+    
+    static func isPrime(_ num: Int) -> Bool {
+        for i in 2..<num {
+            if num % i == 0 {
+                return false
+            }
+        }
+        return true
+    }
+}
